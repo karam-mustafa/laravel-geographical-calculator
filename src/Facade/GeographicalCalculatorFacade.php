9bd7af6e555fdec99ca4cbddeb3e431a3e29198f
karam mustafa <karam2mustafa@gmail.com>
2020-11-21 14:10:58 +0200

    fix getDistance function name

--- src/Facade/GeographicalCalculatorFacade.php
+++ src/Facade/GeographicalCalculatorFacade.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static \KMLaravel\GeographicalCalculator\Classes\GeographicalCalculator initCoordinates($lat1, $lat2, $lon1, $lon2, $options)
- * @method \KMLaravel\GeographicalCalculator\Classes\GeographicalCalculator getLength()
+ * @method \KMLaravel\GeographicalCalculator\Classes\GeographicalCalculator getDistance()
  */
 class GeographicalCalculatorFacade extends Facade
 {
