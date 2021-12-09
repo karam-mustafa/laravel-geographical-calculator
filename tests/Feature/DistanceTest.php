717ecc7bea39173497242671bf0b9f8d5023ba75
karam  mustafa <karam2mustafa@gmail.com>
2021-12-10 00:15:55 +0200

    write package tests

---
+++ tests/Feature/DistanceTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace KMLaravel\GeographicalCalculator\Tests\Feature;
+
+use KMLaravel\GeographicalCalculator\Facade\GeographicalCalculatorFacade;
+use PHPUnit\Framework\TestCase;
+
+class DistanceTest extends TestCase
+{
+    /**
+     * test if the distance equal the correct distance by given coordinate.
+     *
+     * @return void
+     * @throws \Exception
+     */
+    public function test_distance()
+    {
+        $class = GeographicalCalculatorFacade::initCoordinates(22, 33, 37, 40, ['units' => ['km']]);
+        $this->assertEquals([ "km" => 1258.1691302282 ], $class->getDistance());
+    }
+}
