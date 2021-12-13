327cadb8e163e285921b81c7209f37a76716369d
karam  mustafa <karam2mustafa@gmail.com>
2021-12-13 20:21:11 +0200

    add distance_key_prefix key

--- src/Config/geographical_calculator.php
+++ src/Config/geographical_calculator.php
@@ -6,7 +6,7 @@ return [
     |--------------------------------------------------------------------------
     | units values
     |--------------------------------------------------------------------------
-    | your custom units , the initial units its convert from mile to any value.
+    | your custom units, the initial units its convert from mile to any value.
     |
     */
     'units' => [
@@ -16,4 +16,19 @@ return [
         'cm' => (1.609344 * 100),
         'mm' => (1.609344 * 1000 * 1000),
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | distance_key_prefix
+    |--------------------------------------------------------------------------
+    | if you declared more than tow points to resolve their distance,
+    | you will see the result in the following format:
+    | "1-2" => ["km" => "some result"],
+    | "2-3" => ["km" => "some result"],
+    | "3-4" => ["km" => "some result"],
+    | and if you want to set any prefix before each index
+    | you must change the below value to any value you want.
+    |
+    */
+    'distance_key_prefix' => '',
 ];
