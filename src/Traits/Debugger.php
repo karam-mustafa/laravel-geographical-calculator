ddc4f48b05b209c69cef10aadde7cea415d52e06
karam  mustafa <karam2mustafa@gmail.com>
2021-12-13 20:19:56 +0200

    add debugger helper trait, this trait will contains all debug responsibilities

---
+++ src/Traits/Debugger.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace KMLaravel\GeographicalCalculator\Traits;
+
+
+trait Debugger
+{
+
+    /**
+     * @param  mixed  $any
+     *
+     * @return Debugger
+     * @author karam mustafa
+     */
+    private function debug($any)
+    {
+        dd($any);
+
+        return $this;
+    }
+}
