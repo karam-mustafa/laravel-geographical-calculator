130f02ec0fae5a29718c649e3daf692ba4fedb59
karam  mustafa <karam2mustafa@gmail.com>
2021-12-15 01:39:03 +0200

    add new functions

--- src/Facade/GeoFacade.php
+++ src/Facade/GeoFacade.php
@@ -10,6 +10,7 @@ use KMLaravel\GeographicalCalculator\Classes\Geo;
  * @method Geo initCoordinates($lat1, $lat2, $lon1, $lon2, $options)
  * @method Geo getDistance()
  * @method Geo setPoint()
+ * @method Geo getCenter()
  */
 class GeoFacade extends Facade
 {
