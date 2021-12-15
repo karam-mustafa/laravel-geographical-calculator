c923f9fc61d8ae017995561c889d7283d242b74e
karam  mustafa <karam2mustafa@gmail.com>
2021-12-16 00:39:17 +0200

    Added available client functions

--- src/Facade/GeoFacade.php
+++ src/Facade/GeoFacade.php
@@ -6,9 +6,10 @@ use Illuminate\Support\Facades\Facade;
 use KMLaravel\GeographicalCalculator\Classes\Geo;
 
 /**
- * @method Geo initCoordinates($lat1, $lat2, $lon1, $lon2, $options)
  * @method Geo getDistance()
- * @method Geo setPoint()
+ * @method Geo setPoint(array $point)
+ * @method Geo setPoints(array $points)
+ * @method Geo setOptions(array $options)
  * @method Geo getCenter()
  */
 class GeoFacade extends Facade
