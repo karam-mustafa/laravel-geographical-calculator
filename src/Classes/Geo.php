386e588700742a06dd66c45f7e22eff5798675b2
karam  mustafa <karam2mustafa@gmail.com>
2021-12-27 12:51:30 +0200

    wip

--- src/Classes/Geo.php
+++ src/Classes/Geo.php
@@ -24,29 +24,13 @@ class Geo extends AbstractGeo implements GeoInterface
     use Ordering;
 
     /**
-     * all the package feature, this array contains only the name of the feature
-     * if you want to use the feature, then the feature should be such as a getter method.
-     *
-     * @example if you want to use distance => yous should resolve the selected key from this array
-     * and add get key word.
-     * ['distance' => 'getDistance']
-     *
-     * @author karam mustafa
-     * @var array
-     */
-    private $allFeatures = [
-        'Center',
-        'Distance',
-    ];
-
-    /**
      * @inheritDoc
      */
     public function clearResult()
     {
-        // at this time, the Distances trait use the storage
-        // so we check if there is any property called result
-        // we will empty these results
+        // At this time, the Distances trait use the storage,
+        // so we check if there is any property called result,
+        // we will empty these results.
         if (property_exists(__CLASS__, 'result')) {
             $this->clearStoredResults();
             $this->clearPoints();
@@ -63,21 +47,16 @@ class Geo extends AbstractGeo implements GeoInterface
     public function allFeature($callback = null)
     {
 
-        foreach ($this->allFeatures as $feature) {
-            if ($this->inStorage('points')) {
-                $this->setPoint($this->getFromStorage('points'));
-            } else {
-                $this->setInStorage('points', $this->getPoints());
-            }
+        $this->setInStorage('distanceResult' , $this->getDistance())
+            ->clearStoredResults()
+            ->setInStorage('centerResult' , $this->getCenter())
+            ->clearStoredResults();
 
-            $this->setResult([strtolower($feature) => $this->{"get".$feature}()]);
+        $this->setResult([
+            "distance"  => $this->getFromStorage('distanceResult'),
+            "center"  => $this->getFromStorage('centerResult'),
+        ]);
 
-            $this->clearPoints();
-            $this->clearStorage();
-
-        }
-        return $this->getResult(function (Collection $results){
-            return $results->only('center','distance');
-        })->toArray();
+        return $this->getResult();
     }
 }
